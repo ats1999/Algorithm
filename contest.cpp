301691b3a899482a575f4de38a284a2d66acb5dc
Rahul kumar <rkt82224@gmail.com>
2020-12-17 17:14:40 +1100

    checking

---
+++ contest.cpp
@@ -0,0 +1,33 @@
+//============================================================================
+// Name        : .cpp
+// Author      : Rahul (ATS)
+// Version     : 2.0
+// cpp version : c++ 14
+// Copyright   : Everyone can freely use and distribute it.
+// Description :
+// T.C         : O()
+// A.S         : O()
+//============================================================================
+
+#include<bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(false);cin.tie(nullptr)
+
+// driver code
+int main(){
+	// I/O
+	IOS;
+
+    #ifndef ONLINE_JUDGE
+	    freopen("C:\\Users\\Rahul kumar\\desktop\\Algorithm\\input.txt", "r", stdin);
+    #endif
+	cout<<"Started\n";
+	int T;
+	cin>>T;
+	while(T--){
+
+	}
+	cout<<"\nDone:)\n";
+	return 0;
+}
+
