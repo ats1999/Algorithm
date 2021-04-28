18a8ca118fa5df2110bbc503fc5f265451cc8bc5
ats1999 <rkt82224@gmail.com>
2021-04-28 10:40:12 +0530

    test.cpp changes--flag

--- test.cpp
+++ test.cpp
@@ -1,28 +1,26 @@
-//============================================================================
-// Name        : .cpp
-// Author      : Rahul (ATS)
-// Version     : 2.0
-// cpp version : c++ 14
-// Copyright   : Everyone can freely use and distribute it.
-// Description :
-// T.C         : O()
-// A.S         : O()
-//============================================================================
-
 #include<bits/stdc++.h>
 using namespace std;
 #define IOS ios::sync_with_stdio(false);cin.tie(nullptr)
-
+void print(vector<int>&ar){
+    for(int i:ar)
+        cout<<i<<" ";
+    cout<<endl;
+}
+void solve(vector<int>&ar){
+    int n = ar.size();
+    for(int i=0; i<n; i++){
+        vector<int>subar;
+        for(int j=i; j<n; j++){
+            subar.push_back(ar[j]);
+            print(subar);
+        }
+    }
+}
 // driver code
 int main(){
 	// I/O
 	IOS;
-    vector<int>v{1,2,2};
-    do{
-        for(int i:v)
-            cout<<i<<" ";
-            cout<<endl;
-    }while(next_permutation(v.begin(),v.end()));
-
+    vector<int>ar{2,3,6,7};
+    solve(ar);
 	return 0;
 }
