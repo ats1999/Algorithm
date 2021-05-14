6894721dbf40fed661095b65f35a9e9b2cb0eab4
Rahul kumar <rkt82224@gmail.com>
2021-05-14 16:50:14 +0530

    less

--- test.cpp
+++ test.cpp
@@ -1,32 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int getCount(vector<int>&pr, int size, int B){
-    int start = size, end = pr.size()-1;
-
-    while(start <= end){
-        int mid = (start+end)/2;
-        
-    }
-    return 9;
-}
-
 int solve(vector<int> &A, int B) {
-    sort(A.begin(), A.end());
-    
-    vector<int>pre(A.begin(), A.end());
-    for(int i=1; i<A.size(); i++){
-        pre[i] = pre[i]+pre[i-1];
-    }
-    
-    cout<<"Prefix: ";
-    for(int i:pre){
-        cout<<i<<" ";
-    }cout<<endl;
-
-    // for(int size=1; size<=A.size(); size++){
-        getCount(pre,2,B);
-    // }
+    cout<<B<<endl;
     return 0;
 }
 // driver code
