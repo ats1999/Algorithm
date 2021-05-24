e94cb54d3e9d1a647b8355525dea4fd3fc9a2ecf
Rahul kumar <rkt82224@gmail.com>
2021-05-24 09:17:06 +0530

    added rest nodess....

--- cp.cpp
+++ cp.cpp
@@ -13,8 +13,76 @@
 using namespace std;
 #define IOS ios::sync_with_stdio(false);cin.tie(nullptr)
 
-int t;
-// driver code
+class List{
+    public:
+  int val;
+  List *next = NULL;
+  List(int val){
+      this->val = val;
+  }
+};
+List *head=NULL;
+void insert_node(int position, int value) {
+    List *nn = new List(value);
+    if(!head){
+        head = nn;
+        return;
+    }
+    
+    if(position == 1){
+        nn->next = head;
+        head = nn;
+        return;
+    }
+    
+    List *tmp = head;
+    for(int i=1; i<position-1; i++){
+        tmp=tmp->next;
+    }
+    
+    if(tmp->next){
+        nn->next = tmp->next->next;
+        tmp->next = nn;
+    }else{
+        tmp->next = nn;
+    }
+}
+
+void delete_node(int position) {
+    if(position == 1){
+        List *tmp = head;
+        head = head->next;
+        delete tmp;
+        return;
+    }
+    
+    List *tmp=head;
+    for(int i=1; i<position-1; i++){
+        tmp = tmp->next;
+    }
+    
+    if(tmp->next->next){
+        // not last node;
+        List *tmp2 = tmp->next;
+        tmp->next = tmp->next->next;
+        delete tmp2;
+    }else{
+        List *tmp2 = tmp->next;
+        tmp->next = NULL;
+        delete tmp2;
+    }
+}
+
+
+void print_ll() {
+    // Output each element followed by a space
+    List *tmp=head;
+    while(tmp){
+        cout<<tmp->val<<(tmp->next != NULL)?" ":"";
+        tmp = tmp->next;
+    }
+}
+
 int main(){
 	// I/O
 	IOS;
@@ -23,9 +91,9 @@ int main(){
 	    freopen("/home/rahul/Desktop/Algorithm/input.txt", "r", stdin);  
 	    freopen("/home/rahul/Desktop/Algorithm/output.txt", "w", stdout); 
     #endif 
-	cin>>t;
-	while(t--){
-		cout<<"Hello\n";
-	}
+    insert_node(1,5);
+    insert_node(1,9);
+    insert_node(1,7);
+    print_ll();
 	return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,91 +1,30 @@
-#include <bits/stdc++.h>
-using namespace std;
+//============================================================================
+// Name        : .cpp
+// Author      : Rahul (ATS)
+// Version     : 2.0
+// cpp version : c++ 14
+// Copyright   : Everyone can freely use and distribute it.
+// Description :
+// T.C         : O()
+// A.S         : O()
+//============================================================================
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+#include<bits/stdc++.h>
+#include <sys/types.h>
+#include <unistd.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(false);cin.tie(nullptr)
+
+int t;
+// driver code
+int main(){
+	// I/O
+	IOS;
 
     #ifndef ONLINE_JUDGE 
 	    freopen("/home/rahul/Desktop/Algorithm/input.txt", "r", stdin);  
 	    freopen("/home/rahul/Desktop/Algorithm/output.txt", "w", stdout); 
     #endif 
-
-
-    int t;
-    cin>>t;
-    while(t--){
-        vector<string>grid(3);
-        cin>>grid[0]>>grid[1]>>grid[2];
-        
-        // number of times the player won
-        int numWinX=0, numWinO=0,countX=0, countO=0;
-
-        // check for each i
-        for(int i=0; i<3; i++){
-            int colCountX=0, colCountO=0;
-            int rowCountX=0, rowCountO=0;
-            for(int j=0; j<3; j++){
-                // row wise
-                if(grid[i][j] == 'X')
-                    rowCountX++;
-                else if(grid[i][j] == 'O')
-                    rowCountO++;
-
-                // col wise
-                if(grid[j][i] == 'X')
-                    colCountX++;
-                else if(grid[j][i] == 'O')
-                    colCountO++;
-            }
-
-            // increment the number of times X and O wins
-            if(colCountX == 3)
-                numWinX++;
-            else if(colCountO == 3)
-                numWinO++;
-
-            if(rowCountX == 3)
-                numWinX++;
-            else if(rowCountO == 3)
-                numWinO++;
-        }
-
-        // count diagonal
-        int diagX=0, diagO=0;
-        diagX = (grid[0][0] == 'X') + (grid[1][1] == 'X') + (grid[2][2] == 'X');
-        diagO = (grid[0][0] == 'O') + (grid[1][1] == 'O') + (grid[2][2] == 'O');
-
-        if(diagX == 3)
-            numWinX++;
-        else if(diagO == 3)
-            numWinO++;
-
-        // reset
-        diagX=0, diagO=0;
-        diagX = (grid[0][2] == 'X') + (grid[1][1] == 'X') + (grid[2][0] == 'X');
-        diagO = (grid[0][2] == 'O') + (grid[1][1] == 'O') + (grid[2][0] == 'O');
-
-        if(diagX == 3)
-            numWinX++;
-        else if(diagO == 3)
-            numWinO++;
-
-        // count total
-        int totalX=0, totalO = 0;
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if(grid[i][j] == 'X')
-                    totalX++;
-                else if(grid[i][j] == 'O')
-                    totalO++;
-            }
-        }
-        
-        if((numWinX+numWinO)>1 || abs(numWinX-numWinO)>1)
-            cout<<3<<endl;
-        else if(numWinX==1 || numWinO == 1 || (totalO + totalX)==9)
-            cout<<1<<endl;
-        else cout<<2<<endl;
-    }
+	fork(); fork(); cout<<"Yes\n";
 	return 0;
 }
