260cf1178f61c2097dc8751b7e794f17230a9770
Rahul kumar <rkt82224@gmail.com>
2021-05-24 09:21:46 +0530

    local

--- test.cpp
+++ test.cpp
@@ -1,18 +1,30 @@
-#include<bits/stdc++.h>
-using namespace std;
+//============================================================================
+// Name        : .cpp
+// Author      : Rahul (ATS)
+// Version     : 2.0
+// cpp version : c++ 14
+// Copyright   : Everyone can freely use and distribute it.
+// Description :
+// T.C         : O()
+// A.S         : O()
+//============================================================================
 
-int solve(vector<int> &A, int B) {
-    cout<<B<<endl;
-    return 0;
-}
+#include<bits/stdc++.h>
+#include <sys/types.h>
+#include <unistd.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(false);cin.tie(nullptr)
+
+int t;
 // driver code
 int main(){
+	// I/O
+	IOS;
+
     #ifndef ONLINE_JUDGE 
 	    freopen("/home/rahul/Desktop/Algorithm/input.txt", "r", stdin);  
 	    freopen("/home/rahul/Desktop/Algorithm/output.txt", "w", stdout); 
     #endif 
-    vector<int>v{1,2,3,4,5};
-
-    solve(v,4);
-    return 0;
+	fork(); fork(); cout<<"Yes\n";
+	return 0;
 }
