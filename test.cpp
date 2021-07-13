d76f1dd23eccbb3de7e9d239371ae44f13992502
Rahul kumar <rkt82224@gmail.com>
2021-07-13 08:07:40 +0530

    fake

--- test.cpp
+++ test.cpp
@@ -13,91 +13,25 @@
 using namespace std;
 #define IOS ios::sync_with_stdio(false);cin.tie(nullptr)
 
-class List{
-    public:
-  int val;
-  List *next = NULL;
-  List(int val){
-      this->val = val;
-  }
-};
-List *head=NULL;
-void insert_node(int position, int value) {
-    List *nn = new List(value);
-    if(!head){
-        head = nn;
-        return;
-    }
-    
-    if(position == 1){
-        nn->next = head;
-        head = nn;
-        return;
-    }
-    
-    List *tmp = head;
-    for(int i=1; i<position-1; i++){
-        tmp=tmp->next;
-    }
-
-    if(tmp->next){
-        nn->next = tmp->next;
-        tmp->next = nn;
-    }else{
-        tmp->next = nn;
-    }
-}
-
-void delete_node(int position) {
-    if(position == 1){
-        List *tmp = head;
-        head = head->next;
-        delete tmp;
-        return;
-    }
-    
-    List *tmp=head;
-    for(int i=1; i<position-1; i++){
-        tmp = tmp->next;
-    }
-    
-    cout<<tmp->val<<endl;
-    if(tmp->next->next){
-        // not last node;
-        List *tmp2 = tmp->next;
-        tmp->next = tmp->next->next;
-        delete tmp2;
-    }else{
-        List *tmp2 = tmp->next;
-        tmp->next = NULL;
-        delete tmp2;
-    }
-}
-
-
-void print_ll() {
-    // Output each element followed by a space
-    List *tmp=head;
-    while(tmp){
-        cout<<tmp->val<<((tmp->next != NULL)?" ":"");
-        tmp = tmp->next;
-    }
-}
 
 int main(){
 	// I/O
 	IOS;
+    int ar[] = {1,2,3,2,6,8,4,6};
+    int len = 8;
+    unordered_map<int,int>ump;
+    
+    for(int i=0; i<len; i++)
+        ump[ar[i]]++;
 
-    #ifndef ONLINE_JUDGE 
-	    freopen("/home/rahul/Desktop/Algorithm/input.txt", "r", stdin);  
-	    freopen("/home/rahul/Desktop/Algorithm/output.txt", "w", stdout); 
-    #endif 
-    insert_node(1,5);
-    insert_node(1,9);
-    insert_node(2,7);
-    insert_node(3,45);
-    delete_node(2);
-    // insert_node(3,95);
-    print_ll();
+    int idx = 0;
+    for(auto var:ump){
+        if(var.second >= 2)
+            continue;
+        ar[idx++] = var.first;
+    }
+
+    for(int i=0; i<idx; i++)
+        cout<<ar[i]<<" ";
 	return 0;
 }
