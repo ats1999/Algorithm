1218703e60906163c3debd0eb57f010460364d7f
ats1999 <rkt82224@gmail.com>
2021-09-20 09:00:33 +0530

    grid done

--- contest/codechef/START11C/FILLGRID.cpp
+++ contest/codechef/START11C/FILLGRID.cpp
@@ -29,6 +29,18 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (n & 1 && i == j) {
+                    cout<<1<<" ";
+                } else {
+                    cout<<-1<<" ";
+                }
+            }
+            cout<<endl;
+        }
     }
     return 0;
 }
