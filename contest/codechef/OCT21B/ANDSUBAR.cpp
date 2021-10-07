0943f429949e28d2ef00d68632bb5666feb591c2
ats1999 <rkt82224@gmail.com>
2021-10-08 05:15:29 +0530

    cpp snippet

--- contest/codechef/OCT21B/ANDSUBAR.cpp
+++ contest/codechef/OCT21B/ANDSUBAR.cpp
@@ -4,27 +4,41 @@
 // Version     : 2.0
 // cpp version : c++ 17
 // Copyright   : Everyone can freely use and distribute it.
-// Description : https://www.codechef.com/START11C/problems/POSSPEW
+// Description : https://www.codechef.com/OCT21B/problems/ANDSUBAR
 // T.C         : O()
 // A.S         : O()
 //============================================================================
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define IOS ios::sync_with_stdio(false);cin.tie(nullptr)
+#define IOS                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
 
-void file_io(){
+void file_io() {
 #ifndef ONLINE_JUDGE
     freopen("/home/rahul/Desktop/Algorithm/input.txt", "r", stdin);
     freopen("/home/rahul/Desktop/Algorithm/output.txt", "w", stdout);
 #endif
 }
 
-int main(){
-    IOS; file_io(); int t; cin>>t;
-    while(t--){
-        
+void preCompute(int ar[],int n) {
+
+}
+
+int main() {
+    IOS;
+    file_io();
+
+    // pre compute
+    int size=10,ar[10];
+    memset(ar,0,sizeof ar);
+    preCompute(ar,size);
+    
+    int t;
+    cin >> t;
+    while (t--) {
     }
     return 0;
 }
