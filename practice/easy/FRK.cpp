ef413b5f13a067c73ed188666acf40c476872594
ats1999 <rkt82224@gmail.com>
2021-10-29 07:26:33 +0530

    made practice directory

---
+++ practice/easy/FRK.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : .cpp
+// Author      : Rahul (ATS)
+// Version     : 2.0
+// cpp version : c++ 17
+// Copyright   : Everyone can freely use and distribute it.
+// Description : https://www.codechef.com/problems/FRK
+// T.C         : O()
+// A.S         : O()
+//============================================================================
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false);cin.tie(nullptr)
+
+void file_io(){
+#ifndef ONLINE_JUDGE
+    freopen("/home/rahul/Desktop/Algorithm/input.txt", "r", stdin);
+    freopen("/home/rahul/Desktop/Algorithm/output.txt", "w", stdout);
+#endif
+}
+
+int main(){
+    IOS; file_io(); int t; cin>>t;
+    while(t--){
+
+    }
+    return 0;
+}
