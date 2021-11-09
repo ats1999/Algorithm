9275b0bb476e87bbcf35c9535f3a6c079230d478
ats1999 <rkt82224@gmail.com>
2021-11-09 08:17:41 +0530

    -1

---
+++ contest/codechef/NOV21B/HILLSEQ.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : .cpp
+// Author      : Rahul (ATS)
+// Version     : 2.0
+// cpp version : c++ 17
+// Copyright   : Everyone can freely use and distribute it.
+// Description : https://www.codechef.com/NOV21B/problems/HILLSEQ
+// T.C         : O()
+// A.S         : O()
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+
+void file_io() {
+#ifndef ONLINE_JUDGE
+    freopen("/home/rahul/Desktop/Algorithm/input.txt", "r", stdin);
+    freopen("/home/rahul/Desktop/Algorithm/output.txt", "w", stdout);
+#endif
+}
+
+int main() {
+    IOS;
+    file_io();
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+    }
+    return 0;
+}
