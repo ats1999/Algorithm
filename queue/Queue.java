ca6ac316dc093bf3ac3e91b5d5fc5304f7e07504
ats1999 <rkt82224@gmail.com>
2025-04-02 06:42:25 +0530

    added .class into .gitignore

--- queue/Queue.java
+++ queue/Queue.java
@@ -1,2 +1,16 @@
-package queue;public class Queue {
+package queue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queue<T> {
+  private int length;
+  private List<T> queue;
+
+  Queue(int length) {
+    this.length = length;
+    this.queue = new ArrayList<>();
+  }
+
+
 }
